068019a7bd5beb9ae1ab53c900f6659bf91b08ed
Vinicius Campos <viniciuscbalduinoreis@gmail.com>
2024-02-21 20:53:29 -0300

    Ponto Inicial

---
+++ src/main/java/api/caduser/CaduserApplication.java
@@ -0,0 +1,13 @@
+package api.caduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaduserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaduserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/caduser/CaduserApplicationTests.java
@@ -0,0 +1,13 @@
+package api.caduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaduserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
